4442d5df6b6445095a2e6c9843c019d1d84e98b3
Ketsi Emmanuel <kekstudiosofficial@gmail.com>
2024-09-25 00:40:44 +0000

    Initial commit

---
+++ .next/static/HjzuA7WDThM6NgYyYdX6a/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-4f5ff60a85e5706f.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ src/configs/app-global.js
@@ -0,0 +1,42 @@
+export const PROJECT_NAME = 'Uzmart marketplace';
+export const BASE_URL = 'https://api.uzmart.org';
+export const WEBSITE_URL = 'https://uzmart.org';
+export const api_url = BASE_URL + '/api/v1/';
+export const api_url_admin = BASE_URL + '/api/v1/dashboard/admin/';
+export const api_url_admin_dashboard = BASE_URL + '/api/v1/dashboard/';
+export const IMG_URL = '';
+export const MAP_API_KEY = 'AIzaSyAg3aYfrmogsYEuEJXnHE8yHLbuaakiZgo';
+export const export_url = BASE_URL + '/storage/';
+export const example = BASE_URL + '/';
+export const defaultCenter = { lat: 40.7127281, lng: -74.0060152 };
+
+export const VAPID_KEY = '';
+export const API_KEY = 'AIzaSyDLov3GUpZYZ7EWokOYh-fe_9uz6LYxwfU';
+export const AUTH_DOMAIN = 'aztrproject.firebaseapp.com';
+export const PROJECT_ID = 'aztrproject';
+export const STORAGE_BUCKET = 'aztrproject.appspot.com';
+export const MESSAGING_SENDER_ID = '560384748217';
+export const APP_ID = '1:560384748217:web:349515f18c9c2c26680968';
+export const MEASUREMENT_ID = 'G-K7M9Z8HXHV';
+export const DYNAMIC_LINK_DOMAIN = '';
+export const ANDROID_PACKAGE_NAME = '';
+export const IOS_BUNDLE_ID = '';
+
+export const RECAPTCHASITEKEY = '6Lcg1ksqAAAAALbnE9pOvir03RKJVlgGR2v9dO2s';
+
+export const DEMO_SELLER = 107; // seller_id
+export const DEMO_SELLER_UUID = '3566bdf6-3a09-4488-8269-70a19f871bd0'; // seller_id
+export const DEMO_SHOP = 501; // seller_id
+export const DEMO_DELIVERYMAN = 106; // deliveryman_id
+export const DEMO_MANEGER = 114; // maneger_id
+export const DEMO_MODERATOR = 297; // moderator_id
+export const DEMO_ADMIN = 501; // administrator_id
+export const DEMO_WAITER = 108; // waiter_id
+
+export const SUPPORTED_FORMATS = [
+  'image/jpg',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'image/svg',
+];
